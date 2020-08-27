c2dbb424e2c4a00bcbcb26f89fc1ceba88c033bd
Daniel Gruszka <danielgruszka@72-166-196-248.resnet.adams.edu>
2020-08-27 16:49:05 -0600

    assignment 0

--- helloCSCI206.java
+++ helloCSCI206.java
@@ -8,5 +8,8 @@ public class helloCSCI206 {
     public static void main(String[] args) {
 
         System.out.println("Hello CSCI 206 World");
+        System.out.println("Hello CSCI 206 World again");
+        System.out.println("Hello CSCI 206 World again");
+        System.out.println("Hello CSCI 206 World again again again");
     }
 }
